c4e38fd7318ec5296745af3b6dd238bc02201521
noellepaes <noellepaes12345@gmail.com>
2025-03-19 22:52:30 -0300

    finalização da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,58 @@ import { Post } from './componets/Post'
 import { Sidebar } from './componets/Sidebar';
 import styles from './App.module.css';
 import './global.css';
- 
- export function App() {
-   return (
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO '
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
     <div>
+      <Header />
 
-        <Header />
-
-        <div className={styles.wrapper}>
-         <Sidebar />
-         <main>
-           <Post
-             author="Noelle Paes"
-             content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-           />
-           <Post
-             author="Gabriel Buzzi"
-             content="Um novo post muito legal"
-           />
-         </main>
-       </div>
-     </div>
-   )
- }
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
